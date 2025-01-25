d4dd8aefa3e72ddca91ac120eda8a6d9a5915511
ptzt <ignaciotomasrodriguezmillan@gmail.com>
2025-01-25 10:53:32 -0300

    add exercise_27

---
+++ Easy/exercise_27/exercise.js
@@ -0,0 +1,16 @@
+let nums = [0, 1, 2, 2, 3, 0, 4, 2]
+let val = 2
+
+
+var removeElement = function (nums, val) {
+  let k = 0
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[k] = nums[i]
+      k++
+    }
+  }
+  return k
+};
+
+console.log(removeElement(nums, val))
